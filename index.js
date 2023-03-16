9fa4c492e9e696f29e3ba5c25f9b0ad11391e130
Kmzf777 <99519621+Kmzf777@users.noreply.github.com>
2023-03-16 14:26:07 -0300

    .

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let wheel1 = document.getElementById('wheel1');
-
+let wheel2 = document.getElementById('wheel2');
+let car = document.getElementById('car');
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
     wheel1.style.marginLeft = value * -2.5 + 'px';
-    wheel1.style.transformRotate = value * -2.5 + 'deg';
-
+    wheel2.style.marginLeft = value * -2.5 + 'px';
+    car.style.marginLeft = value * -2.5 + 'px';
 });
